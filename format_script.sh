800247516a377390362da5ebdf67a1d4140606e7
alexpeits <alexpeitsinis@gmail.com>
2016-05-04 22:17:18 +0300

    added script to run in case of new workstation

---
+++ format_script.sh
@@ -0,0 +1,16 @@
+#sudo apt-get install vim curl mysql-server-5.7 python-pip python3-pip python-dev python3-dev libmysqlclient-dev git
+
+#mkdir ~/.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+#sudo pip install flask flake8 virtualenv virtualenvwrapper
+#source /usr/local/bin/virtualenvwrapper.sh
+
+#curl -fsSL https://get.docker.com/ | sh
+#sudo usermod -aG docker alex
+
+apm install atom-beautify atom-bootstrap3 autoclose-html autocomplete-python color-picker dash emmet file-icons highlight-selected linter linter-eslint linter-flake8 merge-conflicts minimap minimap-git-diff pigments python-tools rest-client symbols-tree-view terminal-plus
+
+#sudo apt-get install skype zeal chromium-browser
+
+#sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
