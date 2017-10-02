902d7b41de6fea689733c29383918cc7a4f9f99b
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-10-02 23:41:15 +0100

    removed .emacs

--- emacs/myvars.el
+++
@@ -1,8 +0,0 @@
-(setq spacemacs-theme-org-height nil)
-
-(setq my/xtheme 'spacemacs-dark)
-(setq my/theme 'spacemacs-dark)
-(setq my/default-font "DejaVu Sans Mono")
-(setq my/default-font-size 10)
-
-(provide 'myvars)
